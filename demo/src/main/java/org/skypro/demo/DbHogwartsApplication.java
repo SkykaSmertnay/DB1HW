2a39adc221a3eac9522ac5275e0c3a19d614a874
SkykaSmertnay <g925730@gmail.com>
2026-01-17 08:56:22 +0300

    Initial commit

---
+++ demo/src/main/java/org/skypro/demo/DbHogwartsApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbHogwartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbHogwartsApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/org/skypro/demo/DbHogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbHogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
